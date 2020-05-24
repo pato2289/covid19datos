abc97a57960dd86b5b20a5171650fad64a71baaa
patobordenave <patriciofbordenave@gmail.com>
2020-05-24 14:33:01 -0300

    i hate cors

--- src/hooks/useCovidNews.js
+++ src/hooks/useCovidNews.js
@@ -7,7 +7,7 @@ const useCovidNews = () => {
     
     useEffect(() => {
             
-        axios.get('v2/top-headlines?country=ar&apiKey=9e2bcf98ed624677902cc1e037fe9109')
+        axios.get('https://newsapi.org/v2/top-headlines?country=ar&apiKey=9e2bcf98ed624677902cc1e037fe9109')
             .then(res => {
                 console.log("res es: ", res)
                 const noticias = res.data.articles;
